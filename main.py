2885cbf1f366d711cf9c8b4f718c624d5a4e1c86
Brandon Smith <brandonsmith114@gmail.com>
2017-03-21 15:43:00 -0400

    adds rough cuts for 2000, 2004, 2008, 2012 and modifies 2016

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
-import os, csv, re
+from bs4 import BeautifulSoup as bs
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+import csv
 
-headers = ['fips','county','state','year','candidate','vote']
-
-
-
-with open('pres-vote-2012.csv', 'w') as fout:
-    wout = csv.writer(fout)
-    wout.writerow(headers)
-    for file in ['2012_pres_vote_by_county.csv']:
-        print('Processing', file)
-        with open(file, 'r') as fin:
-            reader = csv.reader(fin)
-            reader.__next__()
-            for row in reader:
-                row.insert(3, 2012)
-                wout.writerow(row)
+# shannon county FIPS code changes from 46113 to a 46102 all years use 46113
+# county fips 8014 missing for 2000, it was part of boulder 08013
 
 
+def get_county_level_results(fips, year):
+    records = []
+    base = 'http://uselectionatlas.org/RESULTS/statesub.php?' 
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36',
+        'Accept': 'text/html'
+    }
+    params = {
+        'year': year,
+        'minper':'0',
+        'f':'1',
+        'off':'0',
+        'elect':'0',
+        'fips': fips
+    }
 
-# modifies 2016 to tall and skinny
-# with open('pres-vote.csv', 'w') as fout:
-#     wout = csv.writer(fout)
-#     wout.writerow(headers)
-#     for file in ['2016_pres_vote_by_county.csv']:
-#         print('Processing', file)
-#         with open(file, 'r') as fin:
-#             reader = csv.reader(fin)
-#             reader.__next__()
-#             for row in reader:
-#                 trump = row[0:3]
-#                 clinton = row[0:3]
-#                 trump.extend([2016,'Trump',row[3]])
-#                 clinton.extend([2016,'Clinton', row[4]])
-#                 wout.writerow(clinton)
-#                 wout.writerow(trump)
+    url = base + urlencode(params) 
+    req = Request(url, headers=headers)    
+    soup = bs(urlopen(req).read(), 'lxml')
+    results = soup.select('table.result tbody tr')
+    for result in results:
+        row = result.select('td')
+        records.append([fips, year, row[1].text, row[3].text, int(row[4].text.replace(',',''))])   
+    return records
+
+with open('2004-scrape.csv', 'w', newline='') as fout:
+    with open('data/county/2016-pres-vote-by-county.csv') as fin:
+        reader = csv.reader(fin)
+        reader.__next__() # skips over header
+        writer = csv.writer(fout)
+        for county in reader:
+            try:
+                vote = get_county_level_results(county[0], 2000)
+                if vote:
+                    writer.writerows(vote)
+                else:
+                    print(county[0], ' has no results')
+            except:
+                print(county[0] + ' FAILED')
+
